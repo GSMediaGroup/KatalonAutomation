bfe0170fbbf02bfe8bef945cb7e8070b393bdc49
Runa200 <runaavetisyan200@gmail.com>
2022-02-09 12:09:28 +0400

    Working dev create campaign cases and suite

--- Scripts/Everyday create campaign/dev-Create campaign/Name logic/Script1644239673676.groovy
+++ Scripts/Everyday create campaign/dev-Create campaign/Name logic/Script1644239673676.groovy
@@ -28,8 +28,7 @@ if (Ms.equals('true')) {
 	WebUI.setText(findTestObject('Regular Create campaign/Page_AdRoot Ad Manager - One Place To Manag_7de57e/Page_AdRoot  Create Campaign Steps/input_Campaign name_custom-input'),
 		'devadroot' + RN)
 
-	WebUI.sendKeys(findTestObject('Object Repository/Regular Create campaign/Page_AdRoot  Campaigns/Page_AdRoot  Create Campaign Steps/button_Next Step'),
-		Keys.chord(Keys.ENTER))
+	WebUI.sendKeys(findTestObject('Object Repository/Regular Create campaign/Page_AdRoot  Campaigns/Page_AdRoot  Create Campaign Steps/button_Next Step'), Keys.chord(Keys.ENTER))
 } else {
 	WebUI.click(findTestObject('Object Repository/Regular Create campaign/Page_AdRoot  Campaigns/Page_AdRoot  Create Campaign Steps/button_Next Step'))
 }
---
+++ Test Suites/dev-create campaign suite.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>dev-create campaign suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4b55b56c-df24-4df2-88fe-68306a356df7</testSuiteGuid>
+   <testCaseLink>
+      <guid>1d2022df-130d-4f58-b363-ee2b9659e9df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Everyday create campaign/dev-Create campaign/Dev-Brand campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>650e3bbd-dedc-4cb4-b770-a59981a4bc4a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Everyday create campaign/dev-Create campaign/Dev-Reach campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>57a6f73e-ad9f-42f9-9e06-0ca62f8230cf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Everyday create campaign/dev-Create campaign/Dev-Traffic campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f7d5896-904d-48df-9ac7-075a6f6d1611</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Everyday create campaign/dev-Create campaign/Dev-Video views campaign</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
