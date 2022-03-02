1cd5736f0fd381b261b2d6f1c573c1c905f86b8a
Runa200 <runaavetisyan200@gmail.com>
2022-03-02 19:10:15 +0400

    Dev2 campaign create logic

---
+++ Test Suites/Campaign copy.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Campaign copy</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>90e8d9e7-fc8e-44c4-a578-b2c196ce603e</testSuiteGuid>
+   <testCaseLink>
+      <guid>69f4a394-14ff-42d0-8f8f-aee39b831066</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign - Copy/dev-Create campaign copy/Dev-Brand campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0541bf84-2dc7-44e7-a005-6be2e8a9cedc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign - Copy/dev-Create campaign copy/Dev-Reach campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4b46bf09-b2e7-47f7-8756-41fb4aa279fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign - Copy/dev-Create campaign copy/Dev-Traffic campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2b8efa16-a61f-44d1-83ce-5baad2e30e59</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign - Copy/dev-Create campaign copy/Dev-Video views campaign</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
