af14a8b2824d272b7a705096d776df9bfd667838
Runa200 <runaavetisyan200@gmail.com>
2022-03-11 13:29:51 +0400

    Working app and dev campaign pause logic

---
+++ Test Suites/Suits/SignUp with packages.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SignUp with packages</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0e0be6c1-79af-4f98-b6b3-03664f91b6f8</testSuiteGuid>
+   <testCaseLink>
+      <guid>570f1900-b96d-4375-8100-6ca1a7e937af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/Sign-up with email/Sig-up email-free-from landing</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fb81d497-bc1f-4e14-b93e-f87fdf60e9d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/Sign-up with email/SignUp with email and basic</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2a992f1c-e9ba-4552-ab03-81ef1b0e5f6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/Sign-up with email/SignUp with email and business</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7d571b10-da91-429a-8b3a-436991b3daca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/Sign-up with email/SignUp with email and free</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5845ce96-1742-495f-9521-3b44e16724e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignUp/Sign-up with email/SignUp with email and skip</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Suits/app-create campaign suite.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>app-create campaign suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2a4cf8a8-904e-4318-a3bb-510769afa400</testSuiteGuid>
+   <testCaseLink>
+      <guid>521b6636-4d26-47de-99f1-2ef8b975f374</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign/App-Create campaign/App-Brand campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8facd34d-a614-4616-9cae-b01d5a804627</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign/App-Create campaign/App-Reach campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e48e2350-36d4-44e0-890e-a7a9ace8ba44</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign/App-Create campaign/App-Traffic campaign</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eb145ec1-ca8e-48f9-826d-51b88e58398e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign/App-Create campaign/App-Video views</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>37117d6a-8fe1-40f1-8fec-fd8f8d548851</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Campaign/Create campaign/App-Create campaign/App-Brand campaign - with targeting</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
